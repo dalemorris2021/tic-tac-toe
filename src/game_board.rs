3d4bb447b2b30e8e32a7a3f6afdbb69835633294
Dale Morris <dalemorris2021@gmail.com>
2025-03-21 14:48:00 -0400

    Added logic for checking for a winner

--- src/game_board.rs
+++ src/game_board.rs
@@ -1,10 +1,12 @@
 use std::fmt;
 
-enum Option<T> {
+#[derive(Clone, Copy)]
+pub enum Option<T> {
     None,
     Some(T),
 }
 
+#[derive(Clone, Copy, PartialEq)]
 pub enum Symbol {
     X,
     O,
@@ -20,7 +22,7 @@ impl fmt::Display for Symbol {
     }
 }
 
-type Tile = Option<Symbol>;
+pub type Tile = Option<Symbol>;
 
 impl fmt::Display for Tile {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -32,26 +34,26 @@ impl fmt::Display for Tile {
     }
 }
 
-type Point = (usize, usize);
+pub type Point = (usize, usize);
 
 pub struct GameBoard {
-    rows: [[Tile; 3]; 3],
+    tiles: [[Tile; 3]; 3],
 }
 
 impl fmt::Display for GameBoard {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let rows = &self.rows;
+        let tiles = &self.tiles;
         let repr = format!(
             "{}|{}|{}\n-----\n{}|{}|{}\n-----\n{}|{}|{}",
-            rows[0][0],
-            rows[0][1],
-            rows[0][2],
-            rows[1][0],
-            rows[1][1],
-            rows[1][2],
-            rows[2][0],
-            rows[2][1],
-            rows[2][2],
+            tiles[0][0],
+            tiles[0][1],
+            tiles[0][2],
+            tiles[1][0],
+            tiles[1][1],
+            tiles[1][2],
+            tiles[2][0],
+            tiles[2][1],
+            tiles[2][2],
         );
         write!(f, "{repr}",)
     }
@@ -59,15 +61,19 @@ impl fmt::Display for GameBoard {
 
 impl GameBoard {
     pub fn new() -> GameBoard {
-        let rows = [
+        let tiles = [
             [Option::None, Option::None, Option::None],
             [Option::None, Option::None, Option::None],
             [Option::None, Option::None, Option::None],
         ];
-        GameBoard { rows }
+        GameBoard { tiles }
+    }
+
+    pub fn get_tiles(&self) -> &[[Tile; 3]; 3] {
+        &self.tiles
     }
 
     pub fn put_symbol(&mut self, symbol: Symbol, point: Point) {
-        self.rows[point.0][point.1] = Option::Some(symbol);
+        self.tiles[point.0][point.1] = Option::Some(symbol);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,58 @@ pub mod game_board;
 fn main() {
     let mut game_board = GameBoard::new();
     game_board.put_symbol(Symbol::X, (0, 0));
-    game_board.put_symbol(Symbol::O, (0, 1));
-    game_board.put_symbol(Symbol::X, (0, 2));
+    game_board.put_symbol(Symbol::X, (0, 1));
+    game_board.put_symbol(Symbol::O, (0, 2));
     game_board.put_symbol(Symbol::O, (1, 0));
     game_board.put_symbol(Symbol::X, (1, 1));
-    game_board.put_symbol(Symbol::O, (1, 2));
+    game_board.put_symbol(Symbol::X, (1, 2));
     game_board.put_symbol(Symbol::X, (2, 0));
     game_board.put_symbol(Symbol::O, (2, 1));
-    game_board.put_symbol(Symbol::X, (2, 2));
-
+    game_board.put_symbol(Symbol::O, (2, 2));
     println!("{game_board}");
+
+    let winner = check_win(&game_board);
+    match winner {
+        Option::None => println!("No winner!"),
+        Option::Some(symbol) => println!("{symbol} wins!"),
+    };
+}
+
+fn check_win(game_board: &GameBoard) -> Option<Symbol> {
+    let symbols = [Symbol::X, Symbol::O];
+
+    for symbol in symbols {
+        if check_win_symbol(game_board, &symbol) {
+            return Option::Some(symbol);
+        }
+    }
+
+    Option::None
+}
+
+fn check_win_symbol(game_board: &GameBoard, symbol: &Symbol) -> bool {
+    let tiles = game_board.get_tiles();
+    check_run(&[tiles[0][0], tiles[0][1], tiles[0][2]], symbol)
+        || check_run(&[tiles[1][0], tiles[1][1], tiles[1][2]], symbol)
+        || check_run(&[tiles[2][0], tiles[2][1], tiles[2][2]], symbol)
+        || check_run(&[tiles[0][0], tiles[1][0], tiles[2][0]], symbol)
+        || check_run(&[tiles[0][1], tiles[1][1], tiles[2][1]], symbol)
+        || check_run(&[tiles[0][2], tiles[1][2], tiles[2][2]], symbol)
+        || check_run(&[tiles[0][0], tiles[1][1], tiles[2][2]], symbol)
+        || check_run(&[tiles[0][2], tiles[1][1], tiles[2][0]], symbol)
+}
+
+fn check_run(run: &[Tile; 3], symbol: &Symbol) -> bool {
+    for tile in run {
+        match tile {
+            Option::None => return false,
+            Option::Some(sym) => {
+                if sym != symbol {
+                    return false;
+                }
+            }
+        }
+    }
+
+    true
 }
